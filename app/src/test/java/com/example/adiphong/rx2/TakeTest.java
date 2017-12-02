77fa882f0357d30d0fdb3d829d8789253bfbc936
adiphong <adiphongdev@gmail.com>
2017-12-02 23:56:33 +0700

    Add take

---
+++ app/src/test/java/com/example/adiphong/rx2/TakeTest.java
@@ -0,0 +1,33 @@
+package com.example.adiphong.rx2;
+
+import org.junit.Test;
+
+import io.reactivex.Flowable;
+import io.reactivex.Maybe;
+import io.reactivex.Observable;
+import io.reactivex.Single;
+
+/**
+ * Created by adiphong on 27/11/2017 AD.
+ */
+
+public class TakeTest {
+
+    @Test
+    public void flowableJust() {
+        Flowable.just("Hello", "World")
+                .take(1)
+                .subscribe(
+                        s -> System.out.println("---------------flowable  result " + s)
+                );
+    }
+
+    @Test
+    public void observableJust() {
+        Observable.just("Hello", "World")
+                .take(1)
+                .subscribe(
+                        s -> System.out.println("---------------observable result " + s)
+                );
+    }
+}
