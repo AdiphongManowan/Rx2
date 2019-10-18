e0cc4af26428f5cd67c177ff2202eb0a94de1de1
adiphong <adiphongdev@gmail.com>
2019-10-18 16:12:46 +0700

    migrate to androidX

--- app/src/main/java/com/example/adiphong/rx2/MainActivity.java
+++ app/src/main/java/com/example/adiphong/rx2/MainActivity.java
@@ -1,8 +1,8 @@
 package com.example.adiphong.rx2;
 
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.support.v7.app.AppCompatActivity;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,17 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        OneFragment oneFragment = OneFragment.newInstance();
-        String tag = "OneFragment";
-        replaceFragment(oneFragment, tag);
-    }
-
-    public void replaceFragment(Fragment fragment, String tag) {
-        getSupportFragmentManager()
-                .beginTransaction()
-                .replace(R.id.frame_fragment, fragment, tag)
-                .addToBackStack(tag)
-                .commitAllowingStateLoss();
     }
 
 }
